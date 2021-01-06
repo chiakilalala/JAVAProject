353b6732afe034f5c15827f46148f1aa1f5fd972
chiakilalala <chiaki205102@gmail.com>
2021-01-06 11:52:44 +0800

    first init

---
+++ src/test/java/personal/Person.java
@@ -0,0 +1,10 @@
+package personal;
+
+public class Person {
+
+        public void hello(){
+            System.out.println("hello kotlin");
+        }
+
+
+}
---
+++ src/test/kotlin/meyolin/kotlinjava.kt
@@ -0,0 +1,15 @@
+package meyolin
+
+fun main(args: Array<String>) {
+    //println("hello kotlin")
+    //Human().hello()
+    //ff
+    val h = Human()
+    h.hello();
+}
+
+class Human{
+    fun hello(){
+        println("hello kt")
+    }
+}
